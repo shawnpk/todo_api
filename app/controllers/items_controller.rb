62c51fc38f7355c5642b5760640dbcb54c66d449
Shawn Kearney <shawnpk29@me.com>
2020-07-18 11:14:24 -0600

    Create items controller actions for api

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,43 @@
 class ItemsController < ApplicationController
+  before_action :set_todo
+  before_action :set_item, only: %i[show update destroy]
+
+  def index
+    json_response(@todo.items)
+  end
+
+  def show
+    # @item = @todo.items.find(params[:id])
+    json_response(@item)
+  end
+
+  def create
+    @item = @todo.items.create!(item_params)
+    json_response(@item, :created)
+  end
+
+  def update
+    # @item = @todo.items.find(params[:id])
+    @item.update(item_params)
+    head :no_content
+  end
+
+  def destroy
+    # @item = @todo.items.find(params[:id])
+    @item.destroy
+    head :no_content
+  end
+
+  private
+    def item_params
+      params.permit(:name, :done)
+    end
+
+    def set_item
+      @item = @todo.items.find_by!(id: params[:id]) if @todo
+    end
+
+    def set_todo
+      @todo = Todo.find(params[:todo_id])
+    end
 end
