0dcb3de2fc00467d3768af3ce3ce1561bd64b27a
Shawn Kearney <shawnpk29@me.com>
2020-07-18 10:02:54 -0600

    User json_response method

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,11 +3,11 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      render json: e.message, status: :not_found
+      json_response({ message: e.message }, :not_found)
     end
 
     rescue_from ActiveRecord::RecordInvalid do |e|
-      render json: e.message, status: :unprocessable_entity
+      json_response({ message: e.message }, :unprocessable_entity)
     end
   end
 end
