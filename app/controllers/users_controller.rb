74d35564c0b343cc54ccbc43d4ccccdb6b48b98c
Shawn Kearney <shawnpk29@me.com>
2020-07-20 08:12:03 -0600

    Skip authorize_request method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: %i[create]
+
   def create
     user       = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
