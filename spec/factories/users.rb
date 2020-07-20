62395b9ce35e7b2e79c566e983c9a00daa893173
Shawn Kearney <shawnpk29@me.com>
2020-07-20 07:18:09 -0600

    Update user factory to use password and password_digest

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name            { Faker::Name.name }
-    email           { 'user@example.com' }
-    password_digest { 'fakepass' }
+    name                  { Faker::Name.name }
+    email                 { 'user@example.com' }
+    password              { 'fakepass' }
+    password_confirmation { 'fakepass' }
   end
 end
