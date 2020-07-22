14616c9ae14a475ab943649f21cb421bbbc38e1e
Shawn Kearney <shawnpk29@me.com>
2020-07-22 15:16:06 -0600

    Namespace controllers under v1 module

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -1,43 +1,45 @@
-class ItemsController < ApplicationController
-  before_action :set_todo
-  before_action :set_item, only: %i[show update destroy]
+module V1
+  class ItemsController < ApplicationController
+    before_action :set_todo
+    before_action :set_item, only: %i[show update destroy]
 
-  def index
-    json_response(@todo.items)
-  end
-
-  def show
-    # @item = @todo.items.find(params[:id])
-    json_response(@item)
-  end
-
-  def create
-    @item = @todo.items.create!(item_params)
-    json_response(@item, :created)
-  end
-
-  def update
-    # @item = @todo.items.find(params[:id])
-    @item.update(item_params)
-    head :no_content
-  end
-
-  def destroy
-    # @item = @todo.items.find(params[:id])
-    @item.destroy
-    head :no_content
-  end
-
-  private
-    def item_params
-      params.permit(:name, :done)
+    def index
+      json_response(@todo.items)
     end
 
-    def set_item
-      @item = @todo.items.find_by!(id: params[:id]) if @todo
+    def show
+      # @item = @todo.items.find(params[:id])
+      json_response(@item)
     end
 
-    def set_todo
-      @todo = Todo.find(params[:todo_id])
+    def create
+      @item = @todo.items.create!(item_params)
+      json_response(@item, :created)
     end
+
+    def update
+      # @item = @todo.items.find(params[:id])
+      @item.update(item_params)
+      head :no_content
+    end
+
+    def destroy
+      # @item = @todo.items.find(params[:id])
+      @item.destroy
+      head :no_content
+    end
+
+    private
+      def item_params
+        params.permit(:name, :done)
+      end
+
+      def set_item
+        @item = @todo.items.find_by!(id: params[:id]) if @todo
+      end
+
+      def set_todo
+        @todo = Todo.find(params[:todo_id])
+      end
+  end
 end
--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,36 +1,38 @@
-class TodosController < ApplicationController
-  before_action :set_todo, only: %i[show update destroy]
+module V1
+  class TodosController < ApplicationController
+    before_action :set_todo, only: %i[show update destroy]
 
-  def index
-    @todos = current_user.todos
-    json_response(@todos)
-  end
-
-  def show
-    json_response(@todo)
-  end
-
-  def create
-    @todo = current_user.todos.create!(todo_params)
-    json_response(@todo, :created)
-  end
-
-  def update
-    @todo.update(todo_params)
-    head :no_content
-  end
-
-  def destroy
-    @todo.destroy
-    head :no_content
-  end
-
-  private
-    def set_todo
-      @todo = current_user.todos.find(params[:id])
+    def index
+      @todos = current_user.todos
+      json_response(@todos)
     end
 
-    def todo_params
-      params.permit(:title)
+    def show
+      json_response(@todo)
     end
+
+    def create
+      @todo = current_user.todos.create!(todo_params)
+      json_response(@todo, :created)
+    end
+
+    def update
+      @todo.update(todo_params)
+      head :no_content
+    end
+
+    def destroy
+      @todo.destroy
+      head :no_content
+    end
+
+    private
+      def set_todo
+        @todo = current_user.todos.find(params[:id])
+      end
+
+      def todo_params
+        params.permit(:title)
+      end
+  end
 end
